c609834a89ea1036478f20ac1accbbc36d330da1
SergioToddePXL <sergio.todde@student.pxl.be>
2025-11-19 23:10:53 +0100

    responsiviteit aangepast bij registreer formulier en dubbele tekst verwijdert

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -73,6 +73,7 @@ export default {
 .register-view {
   min-height: 100vh;
   display: flex;
+  justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
   padding: 2rem 0;
